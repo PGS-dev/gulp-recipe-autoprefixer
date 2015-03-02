648802078b6a384e70efc31115905197985fcbbe
pgrabarz <pgrabarz@pgs-soft.com>
2015-03-02 09:40:41 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = function ($, config, sources) {
+    var _ = $.lodash;
+
+    var conf = _.merge(config.autoprefixer || {}, {
+        versions: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1', 'ie 9'],
+        order: 100,
+        dev: true,
+        prod: true
+    });
+
+    var pipeDef = [conf.order, $.lazypipe().pipe($.autoprefixer, conf.versions)];
+    return {
+        pipes: {
+            processCss: conf.prod ? pipeDef : undefined,
+            devProcessCss: conf.dev ? pipeDef : undefined
+        }
+    };
+};
