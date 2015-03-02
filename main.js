d039b6e91a9068c82762d77bdb8a13ab82a119fc
pgrabarz <pgrabarz@pgs-soft.com>
2015-03-02 09:55:43 +0100

    fix wrong config reading order

--- main.js
+++ main.js
@@ -3,12 +3,12 @@
 module.exports = function ($, config, sources) {
     var _ = $.lodash;
 
-    var conf = _.merge(config.autoprefixer || {}, {
+    var conf = _.merge({
         versions: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1', 'ie 9'],
         order: 100,
         dev: true,
         prod: true
-    });
+    }, config.autoprefixer);
 
     var pipeDef = [conf.order, $.lazypipe().pipe($.autoprefixer, conf.versions)];
     return {
